95c720346ef07028952728955b2641c2e03a69fc
ArikSetyawan <setyawanarik@gmail.com>
2023-06-23 15:31:11 +0700

    add relationship

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 import uvicorn
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
 
 # Models
 
@@ -15,6 +16,13 @@ class User(Base):
     username = Column(String, unique=True)
     password = Column(String)
     photo = Column(String, nullable=True)
+    hobbies = relationship('Hobby', backref='user')
+
+class Hobby(Base):
+    __tablename__ = 'hobby'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    name = Column(String)
 
 engine = create_engine('sqlite:///users.db', echo=True)
 Session = sessionmaker(bind=engine)
@@ -24,17 +32,19 @@ app = FastAPI()
 
 @app.get('/')
 async def index():
-    # get all User
+    # get all User left join hobby
     users = session.query(User).all()
+    for item in users:
+        print(item.username, item.photo, item.hobbies)
     return {"message": users}
 
 @app.post('/user')
-async def create_user(request: Request):
+async def create_user(request: Request, response: Response):
     # Create User
     data = await request.json()
     if "username" not in data:
-        return {"message": "username is required"},400
-    print(data)
+        response.status_code = 400
+        return {"message": "username is required"}
     return {"message": "User created"}
 
 if __name__ == '__main__':
